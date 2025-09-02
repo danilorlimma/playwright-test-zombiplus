58d40a6941b59de56517f44a831ec5b19dceaf30
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2025-09-02 10:29:37 -0300

    Desacoplando steps

--- tests/pages/MoviesPage.js
+++ tests/pages/MoviesPage.js
@@ -9,15 +9,24 @@ export class MoviesPage {
         await this.page.waitForLoadState('networkidle')
         await expect(this.page).toHaveURL(/.*admin/)
     }
-    async create(title, overview, company, release_year) {
+    async goForm() {
         await this.page.locator('a[href$="register"]').click()
+    }
+    async submit() {
+        await this.page.getByRole('button', { name: 'Cadastrar' }).click()
+    }
+    async create(title, overview, company, release_year) {
+        await this.goForm()
         await this.page.getByLabel('Titulo do filme').fill(title)
         await this.page.getByLabel('Sinopse').fill(overview)
         await this.page.locator('#select_company_id .react-select__indicator').click()
         await this.page.locator('.react-select__option').filter({ hasText: company }).click()
         await this.page.locator('#select_year .react-select__indicator').click()
         await this.page.locator('.react-select__option').filter({ hasText: release_year }).click()
-        await this.page.getByRole('button',{name:'Cadastrar'}).click()
-        
+        await this.submit()
+
+    }
+    async alertHaveText(target){
+        await expect(this.page.locator('.alert')).toHaveText(target)
     }
 }
