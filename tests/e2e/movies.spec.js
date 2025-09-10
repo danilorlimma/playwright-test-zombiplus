c42f33d5494439593e1754c3a746aea8b0b3b25b
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2025-09-09 21:01:15 -0300

    teste com dependencia

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -12,6 +12,14 @@ test('Deve poder cadastrar um novo filme', async ({ page }) => {
     await page.toast.containText('Cadastro realizado com sucesso!')
 })
 
+test('Não deve cadastrar quando o título é duplicado', async ({ page }) => {
+    const movie = data.create
+
+    await page.login.do('admin@zombieplus.com', 'pwd123','Admin')
+    await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year, movie.cover, movie.featured)
+    await page.toast.containText('Oops!Este conteúdo já encontra-se cadastrado no catálogo+')
+})
+
 test('Não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({page}) => {
     await page.login.do('admin@zombieplus.com', 'pwd123','Admin')
 
